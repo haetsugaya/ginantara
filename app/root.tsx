6550aecf75dd8725019fcb1c65cabe3c8ab8b47a
aag <aryaanjar@outlook.com>
2024-10-17 15:43:28 +0800

    modular

--- app/root.tsx
+++ app/root.tsx
@@ -1,42 +1,39 @@
 import {
+  json,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
   useRouteError,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
+import {
+  useGoogleTagManager,
+  useGoogleTagManagerNoScript,
+} from "./libs/functions/gtm";
+import { combineLinks } from "./libs/functions/links";
+import { fontLinks } from "./libs/constant/fonts";
+import { styleLinks } from "./libs/constant/style";
 
-import tailwind from "~/tailwind.css?url";
-import { useGoogleTagManager, useGoogleTagManagerNoScript } from "./utils/gtm";
+export async function loader() {
+  return json({
+    ENV: {
+      GTM_CONTAINER_IDS: process.env.GTM_CONTAINER_ID?.split(",") || [],
+    },
+  });
+}
 
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap",
-  },
-  {
-    rel: "stylesheet",
-    href: tailwind,
-  },
-];
+export const links: LinksFunction = () => combineLinks(fontLinks, styleLinks);
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const gtmContainerIds = process.env.GTM_CONTAINER_IDS?.split(",") || [];
+  const data = useLoaderData<typeof loader>();
+  const gtmId = data.ENV.GTM_CONTAINER_IDS;
 
-  // Only call the GTM functions if the array or env is not empty
-  if (gtmContainerIds.length > 0) {
-    useGoogleTagManager(gtmContainerIds);
-    useGoogleTagManagerNoScript(gtmContainerIds);
-  }
-  
+  useGoogleTagManager(gtmId);
+  useGoogleTagManagerNoScript(gtmId);
+
   return (
     <html lang="en">
       <head>
